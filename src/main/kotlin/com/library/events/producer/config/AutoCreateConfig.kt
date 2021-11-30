09b3e7831710e9c3658d388e008ed46aee89b70c
Caio Serra <caio_mello@id.uff.br>
2021-11-30 20:59:59 -0300

    AutoCreateConfig.kt Adiciona comando profile para local

--- src/main/kotlin/com/library/events/producer/config/AutoCreateConfig.kt
+++ src/main/kotlin/com/library/events/producer/config/AutoCreateConfig.kt
@@ -3,9 +3,11 @@ package com.library.events.producer.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.apache.kafka.clients.admin.NewTopic
+import org.springframework.context.annotation.Profile
 import org.springframework.kafka.config.TopicBuilder
 
 @Configuration
+@Profile("local")
 class AutoCreateConfig {
 
     @Bean
