40fe35a6cb3246304915ab857a29c52861d97ba6
Caio Serra <caio_mello@id.uff.br>
2021-11-30 21:36:58 -0300

    LibraryEventProducer.kt Cria classe componente para lidar com o kafka

---
+++ src/main/kotlin/com/library/events/producer/producer/LibraryEventProducer.kt
@@ -0,0 +1,49 @@
+package com.library.events.producer.producer
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.library.events.producer.domain.LibraryEvent
+import lombok.extern.slf4j.Slf4j
+import org.slf4j.Logger
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.support.SendResult
+import org.springframework.stereotype.Component
+import org.springframework.util.concurrent.ListenableFuture
+
+@Component
+@Slf4j
+class LibraryEventProducer {
+
+    @Autowired
+    lateinit var kafkaTemplate: KafkaTemplate<Int, String>
+
+    @Autowired
+    lateinit var objectMapper: ObjectMapper
+
+    @Autowired
+    lateinit var log: Logger
+
+    fun sendLibraryEvent(libraryEvent: LibraryEvent) {
+        val key = libraryEvent.libraryEventId
+        val value = this.objectMapper.writeValueAsString(libraryEvent.book)
+        val listenableFuture: ListenableFuture<SendResult<Int,String>> = kafkaTemplate.sendDefault(key, value)
+        listenableFuture.addCallback(
+            { result:SendResult<Int,String>? -> handleSuccess(key, value, result) },
+            {ex: Throwable -> handleFailure(key, value, ex)}
+        )
+    }
+
+    private fun handleFailure(key: Int, value: String?, ex: Throwable) {
+        log.error("Error sending the message for the key $key value $value and the exception is ${ex.message}")
+        try{
+            throw ex
+        } catch (throwable:Throwable){
+            log.error("Error in OnFailure: ${throwable.message}")
+        }
+    }
+
+
+    private fun handleSuccess(key: Int, value: String, sendResult: SendResult<Int, String>?) {
+        log.info("Message sent successfully for the key $key value $value, partition is ${sendResult?.recordMetadata?.partition()} ")
+    }
+}
