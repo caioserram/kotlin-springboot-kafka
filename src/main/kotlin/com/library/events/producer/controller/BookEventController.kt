714e46c306e6e72f1882357919f7f4cd1d06f8f3
Caio Serra <caio_mello@id.uff.br>
2021-11-30 21:52:11 -0300

    BookEventController.kt adiciona envio de mensagem utilizando o producer

--- src/main/kotlin/com/library/events/producer/controller/BookEventController.kt
+++ src/main/kotlin/com/library/events/producer/controller/BookEventController.kt
@@ -2,6 +2,8 @@ package com.library.events.producer.controller
 
 import com.library.events.producer.domain.Book
 import com.library.events.producer.domain.LibraryEvent
+import com.library.events.producer.producer.LibraryEventProducer
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -13,9 +15,14 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping(value=["/v1/libraryevent"])
 class BookEventController {
 
+    @Autowired
+    lateinit var libraryEventProducer: LibraryEventProducer
+
     @PostMapping()
     fun createBookEvent(@RequestBody libraryEvent: LibraryEvent): ResponseEntity<LibraryEvent>{
         print(libraryEvent)
+
+        libraryEventProducer.sendLibraryEvent(libraryEvent)
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent)
     }
 }
