3d6343cd0a42fd10b9b0029fe6f91f2bc0721c22
Caio Serra <caio_mello@id.uff.br>
2021-11-30 21:35:41 -0300

    AutoCreateConfig.kt Adiciona comentários para estudo

--- src/main/kotlin/com/library/events/producer/config/AutoCreateConfig.kt
+++ src/main/kotlin/com/library/events/producer/config/AutoCreateConfig.kt
@@ -2,14 +2,15 @@ package com.library.events.producer.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.apache.kafka.clients.admin.NewTopic
 import org.springframework.context.annotation.Profile
 import org.springframework.kafka.config.TopicBuilder
 
 @Configuration
+// Define que código deve ser executado apenas localmente
 @Profile("local")
 class AutoCreateConfig {
 
+    // Cria tópico automaticamente
     @Bean
     fun libraryEvents() =
         TopicBuilder.name("library-events")
