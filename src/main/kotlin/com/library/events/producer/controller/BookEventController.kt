b6fd317fcbd30be2bc19470090ad9dfb74bd44b0
Caio Serra <caio_mello@id.uff.br>
2021-12-01 20:56:23 -0300

    BookEventController.kt adiciona chamada de função sícrona ao controller ao contro

--- src/main/kotlin/com/library/events/producer/controller/BookEventController.kt
+++ src/main/kotlin/com/library/events/producer/controller/BookEventController.kt
@@ -1,8 +1,9 @@
 package com.library.events.producer.controller
 
-import com.library.events.producer.domain.Book
 import com.library.events.producer.domain.LibraryEvent
 import com.library.events.producer.producer.LibraryEventProducer
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -18,11 +19,17 @@ class BookEventController {
     @Autowired
     lateinit var libraryEventProducer: LibraryEventProducer
 
+    var log: Logger = LoggerFactory.getLogger("BookEventController")
+
     @PostMapping()
     fun createBookEvent(@RequestBody libraryEvent: LibraryEvent): ResponseEntity<LibraryEvent>{
-        print(libraryEvent)
-
-        libraryEventProducer.sendLibraryEvent(libraryEvent)
+        log.info("Before send library event")
+        // Due to its asynchronous behavior, method will be considered successful even though the message did not get to
+        // the broker
+//        libraryEventProducer.sendLibraryEventAsync(libraryEvent)
+        val sendResult = libraryEventProducer.sendLibraryEvent(libraryEvent)
+        log.info("SendResult is ${sendResult.toString()}")
+        log.info("After send library event")
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent)
     }
 }
