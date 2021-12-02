ecf1765e9cd204f92dc24f35cdcd1cb3d5b00407
Caio Serra <caio_mello@id.uff.br>
2021-12-01 21:20:31 -0300

    LibraryEventController.kt altera nome do arquivo

--- src/main/kotlin/com/library/events/producer/controller/LibraryEventController.kt
+++ src/main/kotlin/com/library/events/producer/controller/LibraryEventController.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping(value=["/v1/libraryevent"])
-class BookEventController {
+class LibraryEventController {
 
     @Autowired
     lateinit var libraryEventProducer: LibraryEventProducer
